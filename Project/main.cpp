7afaa11da36e7c91cfe727660266c0d1799ebb8f
Hwangsangjin <hwangsangjin88@gmail.com>
2023-09-03 19:41:24 +0900

    메모리를 자동으로 해제하는 동적 배열 클래스

--- Project/main.cpp
+++ Project/main.cpp
@@ -2,22 +2,42 @@
 
 using namespace std;
 
-int main()
+class DynamicArray
 {
-	// 동적 배열 할당 및 초기화
-	int* Ptr = new int[3] {};
-	Ptr[0] = 10;
-	Ptr[1] = 20;
-
-	// 포인터를 배열처럼 사용
-	for (int i = 0; i < 3; i++)
+public:
+	DynamicArray(int N)
+		: Size(N)
 	{
-		std::cout << Ptr[i] << std::endl;
+		Ptr = new int[Size] {};
+	};
+
+	~DynamicArray()
+	{
+		delete[] Ptr;
+		Ptr = nullptr;
 	}
 
-	// 동적 배열 해제
-	delete[] Ptr;
-	Ptr = nullptr;
+	unsigned int size() { return Size; }
+	int& operator[] (const int i) { return Ptr[i]; }
+	const int& operator[] (const int i) const { return Ptr[i]; }
+
+private:
+	unsigned int Size;
+	int* Ptr;
+};
+
+int main()
+{
+	DynamicArray DA(5);
+	DA[0] = 10;
+	DA[1] = 20;
+	DA[2] = 30;
+
+	for (int i = 0; i < DA.size(); i++)
+	{
+		cout << DA[i] << " ";
+	}
+	cout << endl;
 
 	return 0;
 }

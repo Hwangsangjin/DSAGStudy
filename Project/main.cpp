c7141c8427816093a22fbeb939396fa235d439c4
Hwangsangjin <hwangsangjin88@gmail.com>
2023-09-22 19:18:20 +0900

    std::stack

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,29 +1,11 @@
 ﻿#include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
-template <typename T>
-class Stack
-{
-public:
-	Stack() {}
-
-	void push(const T& e) { List.push_back(e); }
-	void pop() { List.pop_back(); }
-	const T& top() const { return List.back(); }
-
-	bool empty() const { return List.empty(); }
-	int size() const { return List.size(); }
-
-private:
-	vector<T> List;
-};
-
-
 int main()
 {
-	Stack<int> STK;
+	stack<int> STK;
 	STK.push(10);
 	STK.push(20);
 	STK.push(30);

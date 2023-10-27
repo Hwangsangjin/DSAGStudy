0001ad72fd9679bcef5fddd7ce166c540df23c3b
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2023-10-27 21:46:57 +0900

    환형 큐의 구현

--- Project/main.cpp
+++ Project/main.cpp
@@ -3,22 +3,86 @@
 
 using namespace std;
 
+#define MAX_QUEUE 10
+
+template <typename T>
+class CircularQueue
+{
+public:
+	CircularQueue(int size = MAX_QUEUE)
+	{
+		Array = new T[size];
+		Capacity = size;
+		Count = 0;
+		FrontIndex = 0;
+		RearIndex = -1;
+	}
+	~CircularQueue()
+	{
+		delete[] Array;
+	}
+
+	void enqueue(const T& e)
+	{
+		if (full())
+		{
+			cout << "Overflow error!" << endl;
+			return;
+		}
+
+		RearIndex = (RearIndex + 1) % Capacity;
+		Array[RearIndex] = e;
+		Count++;
+	}
+
+	void dequeue()
+	{
+		if (empty())
+		{
+			cout << "Underflow error!" << endl;
+			return;
+		}
+
+		FrontIndex = (FrontIndex + 1) % Capacity;
+		Count--;
+	}
+
+	const T& front() const { return Array[FrontIndex]; }
+
+	bool empty() const { return Count == 0; }
+	int full() const { return Count == Capacity; }
+	int size() const { return Count; }
+
+private:
+	T* Array;
+	int FrontIndex;
+	int RearIndex;
+	int Count;
+	int Capacity;
+};
+
 int main()
 {
-	queue<int> Q;
-	Q.push(10);
-	Q.push(20);
-	Q.push(30);
-	Q.pop();
+	CircularQueue<int> Q(5);
 
-	cout << Q.front() << endl;
-	Q.push(40);
+	Q.enqueue(10);
+	Q.enqueue(20);
+	Q.enqueue(30);
+	Q.enqueue(40);
+	Q.enqueue(50);
+
+	Q.dequeue();
+	Q.dequeue();
+
+	Q.enqueue(60);
+	Q.enqueue(70);
+	Q.enqueue(80);
 
 	while (!Q.empty())
 	{
-		const auto& e = Q.front();
+		auto& e = Q.front();
 		cout << e << " ";
-		Q.pop();
+		Q.dequeue();
 	}
 
 	return 0;

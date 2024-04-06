8f51349cd519817ec6cba1dd842d9af187e1a0ce
Hwangsangjin <hwangsangjin88@gmail.com>
2024-04-06 16:20:52 +0900

    재귀 알고리즘

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,89 +1,21 @@
 ﻿#include <iostream>
-#include <queue>
 
-using namespace std;
-
-#define MAX_QUEUE 10
-
-template <typename T>
-class CircularQueue
+int Sum(int n)
 {
-public:
-	CircularQueue(int size = MAX_QUEUE)
+	if (n == 1)
 	{
-		Array = new T[size];
-		Capacity = size;
-		Count = 0;
-		FrontIndex = 0;
-		RearIndex = -1;
+		return 1;
 	}
-	~CircularQueue()
+	else
 	{
-		delete[] Array;
+		return n + Sum(n - 1);
 	}
-
-	void enqueue(const T& e)
-	{
-		if (full())
-		{
-			cout << "Overflow error!" << endl;
-			return;
-		}
-
-		RearIndex = (RearIndex + 1) % Capacity;
-		Array[RearIndex] = e;
-		Count++;
-	}
-
-	void dequeue()
-	{
-		if (empty())
-		{
-			cout << "Underflow error!" << endl;
-			return;
-		}
-
-		FrontIndex = (FrontIndex + 1) % Capacity;
-		Count--;
-	}
-
-	const T& front() const { return Array[FrontIndex]; }
-
-	bool empty() const { return Count == 0; }
-	int full() const { return Count == Capacity; }
-	int size() const { return Count; }
-
-private:
-	T* Array;
-	int FrontIndex;
-	int RearIndex;
-	int Count;
-	int Capacity;
-};
+}
 
 int main()
 {
-	CircularQueue<int> Q(5);
-
-	Q.enqueue(10);
-	Q.enqueue(20);
-	Q.enqueue(30);
-	Q.enqueue(40);
-	Q.enqueue(50);
-
-	Q.dequeue();
-	Q.dequeue();
-
-	Q.enqueue(60);
-	Q.enqueue(70);
-	Q.enqueue(80);
-
-	while (!Q.empty())
-	{
-		auto& e = Q.front();
-		cout << e << " ";
-		Q.dequeue();
-	}
+	int result = Sum(5);
+	std::cout << result << std::endl;
 
 	return 0;
 }

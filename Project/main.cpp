7f17bade929569582a998e64d3ee120a230d4de8
Hwangsangjin <hwangsangjin88@gmail.com>
2024-04-26 08:10:48 +0900

    순차 탐색과 이진 탐색

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,92 +1,62 @@
 ﻿#include <iostream>
 #include <algorithm>
-#include <functional>
-#include <string>
-#include <vector>
 
-class Person
+bool linear_search(int data[], int n, int target)
 {
-public:
-	std::string name;
-	int age;
-
-	bool operator<(const Person& p) const
+	for (int i = 0; i < n; i++)
 	{
-		return age < p.age;
+		if (data[i] == target)
+		{
+			return true;
+		}
 	}
 
-	void Print() const
-	{
-		std::cout << name << ", " << age << std::endl;
-	}
-};
+	return false;
+}
 
-bool abs_cmp(const int& a, const int& b)
+bool binary_search(int data[], int n, int target)
 {
-	return std::abs(a) < std::abs(b);
+	int lower = 0;
+	int upper = n - 1;
+
+	if (data[lower] > target || data[upper] < target)
+	{
+		return false;
+	}
+
+	while (lower <= upper)
+	{
+		int mid = (lower + upper) / 2;
+		
+		if (data[mid] == target)
+		{
+			return true;
+		}
+		else if (data[mid] < target)
+		{
+			lower = mid + 1;
+		}
+		else
+		{
+			upper = mid - 1;
+		}
+	}
+
+	return false;
 }
 
 int main()
 {
-	// 배열의 정렬
-	int arr[5] = {4, 2, 3, 5, 1};
-	std::sort(arr, arr + 5); // 오름 차순, [1, 2, 3, 4, 5]
-	//std::sort(std::begin(arr), std::end(arr)); // std::begin(), std::end()
-	//std::sort(std::begin(arr), std::end(arr), std::greater<>()); // 내림 차순, [5, 4, 3, 2, 1]
+	int data[] = {1, 2, 3, 5, 7, 10, 13, 15, 18, 23, 25, 27, 28, 30, 33};
+	int target = 23;
 
-	for (const auto& e : arr)
-	{
-		std::cout << e << " ";
-	}
-	std::cout << std::endl;
+	bool res1 = linear_search(data, std::size(data), target);
+	bool res2 = binary_search(data, std::size(data), target);
+	bool res3 = std::binary_search(std::begin(data), std::end(data), target);
 
-	// 벡터의 정렬
-	std::vector<std::string> vec = {"orange", "banana", "apple", "lemon"};
-	std::sort(vec.begin(), vec.end(), std::greater<>());
-	//std::sort(vec.begin(), vec.end(), std::less<>()); // ["apple", "banana", "lemon", "orange"]
-	//std::sort(vec.begin(), vec.end(), std::greater<>()); // ["orange", "lemon", "banana", apple"]
-	//std::sort(begin(vec), end(vec));
-
-	for (const auto& e : vec)
-	{
-		std::cout << e << " ";
-	}
-	std::cout << std::endl;
-
-	// 비교 방법 지정
-	struct AbsCmp
-	{
-		inline bool operator()(int a, int b) const
-		{
-			return std::abs(a) < std::abs(b);
-		}
-	};
-
-	std::vector<int> nums = {10, 2, -3, 5, 7};
-	std::sort(nums.begin(), nums.end(), AbsCmp()); // [2, -3, 5, 7, 10]
-	std::sort(nums.begin(), nums.end(), abs_cmp); // [2, -3, 5, 7, 10]
-	std::sort(nums.begin(), nums.end(), [](int a, int b) { return std::abs(a) < std::abs(b); }); // [2, -3, 5, 7, 10]
-
-	for (const auto& e : nums)
-	{
-		std::cout << e << " ";
-	}
-	std::cout << std::endl;
-
-	// 비교 연산자를 오버로딩한 클래스 객체의 정렬
-	std::vector<Person> people;
-	people.push_back({"Amelia", 29});
-	people.push_back({"Noah", 25});
-	people.push_back({"Olivia", 31});
-	people.push_back({"Sophia", 40});
-	people.push_back({"George", 35});
-
-	std::sort(people.begin(), people.end());
-
-	for (const auto& person : people)
-	{
-		person.Print();
-	}
+	std::cout << res1 << std::endl;
+	std::cout << res2 << std::endl;
+	std::cout << res3 << std::endl;
 
 	return 0;
 }
